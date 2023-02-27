b45dbe841e852ff9a782a78f990b3fc3e63da5f3
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2023-02-27 14:19:05 +0000

    Add Root controller

---
+++ src/main/java/uk/twl/controller/RootController.java
@@ -0,0 +1,34 @@
+package uk.twl.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+/**
+ * Default endpoints per application.
+ */
+@RestController
+public class RootController {
+
+    public static final String WELCOME_MESSAGE = "Welcome to %s";
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    /**
+     * Root GET endpoint.
+     *
+     * <p>Azure application service has a hidden feature of making requests to root endpoint when
+     * "Always On" is turned on.
+     * This is the endpoint to deal with that and therefore silence the unnecessary 404s as a response code.
+     *
+     * @return Welcome message from the service.
+     */
+    @GetMapping("/")
+    public ResponseEntity<String> welcome() {
+        return ok(String.format(WELCOME_MESSAGE, applicationName));
+    }
+}
